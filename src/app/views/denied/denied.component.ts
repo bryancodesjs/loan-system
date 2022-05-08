807664927d4fee18a67417e6557edd3f47d7112f
bryancodesjs <72945681+bryancodesjs@users.noreply.github.com>
2022-05-08 17:46:55 -0400

    denied component completed

--- src/app/views/denied/denied.component.ts
+++ src/app/views/denied/denied.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoanService } from 'src/app/services/loan.service';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-denied',
   templateUrl: './denied.component.html',
   styleUrls: ['./denied.component.scss']
 })
-export class DeniedComponent implements OnInit {
+export class DeniedComponent {
 
-  constructor() { }
-
+  constructor(private _LoanService: LoanService) { }
+  deniedLoans:any = [];
   ngOnInit(): void {
+    this.retrieveAll();
   }
-
+  retrieveAll(){
+    this._LoanService.getAll().snapshotChanges().pipe(
+      map(changes => changes.map( c =>
+        ({ key: c.payload.key, ...c.payload.val() })))
+    ).subscribe (data => {
+      var toFilter = data;
+      //only show the requests which arent active and arent denied
+      var filtered = toFilter.filter(a => a.active == false && a.denied == true && a.completed == false);
+      //console.log(filtered);
+      this.deniedLoans = filtered;
+    });
+  }
 }
