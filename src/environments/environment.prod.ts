74a1ff4aa9f2bd7c0bdc555a1a699ddc5efb6f8a
bryancodesjs <72945681+bryancodesjs@users.noreply.github.com>
2022-05-08 16:59:46 -0400

    update upstream

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDusUHkV1hS0plJKxu6ERrkk67Y1gxlwQM",
+    authDomain: "loan-system-86e6a.firebaseapp.com",
+    projectId: "loan-system-86e6a",
+    storageBucket: "loan-system-86e6a.appspot.com",
+    messagingSenderId: "1023670610620",
+    appId: "1:1023670610620:web:84c2175126d13a9df3beb9",
+    measurementId: "G-99C15EL6MD"
+  }
 };
